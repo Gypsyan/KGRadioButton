33d1c8ccf59ae7803b13ebdfb5619256d363f183
Ananth <agirijak@in.ibm.com>
2017-12-08 15:32:49 +0530

    added auto release

--- KGRadioButton.podspec
+++ KGRadioButton.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "KGRadioButton"
-  s.version      = "1.0.0"
+  s.version      = "1.2.0"
   s.summary      = "Add Ripple action around a button"
   s.homepage     = "https://github.com/Gypsyan/KGRadioButton"
   s.license      = 'Apache License, Version 2.0'
---
+++ scripts/release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script is used by Travis-CI to automatically release new versions of BMSPush.
+# First, we check if the version specified in the .podspec does not already exist as a git tag.
+# If the version does not exist yet, we add a git tag for this new version and publish to Cocoapods.
+
+set -ev
+cd ~/Documents
+# GITHUB_TOKEN required for Travis to have permissions to push to the KGRadioButton repository
+git clone https://Gypsyan:${GITHUB_TOKEN}@github.com/Gypsyans/KGRadioButton.git
+cd KGRadioButton
+git remote rm origin
+git remote add origin https://Gypsyan:${GITHUB_TOKEN}@github.com/Gypsyans/KGRadioButton.git
+version=$(grep -o 'version.*=.*[0-9]' KGRadioButton.podspec | cut -f 2 -d "'")
+git fetch --tags
+if [[ ! "$(git tag)" =~ "${version}" ]]; then
+	echo "Publishing new version ${version} ";
+	git tag $version;
+	git push origin --tags;
+	pod trunk push --allow-warnings;
+fi
