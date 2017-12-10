b557882207f48d54c08573de5efc91431e772672
Ananth <agirijak@in.ibm.com>
2017-12-10 12:08:58 +0530

    Updated the release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,10 +7,10 @@
 set -ev
 cd ~/Documents
 # GITHUB_TOKEN required for Travis to have permissions to push to the KGRadioButton repository
-git clone https://Gypsyan:${GITHUB_TOKEN}@github.com/Gypsyans/KGRadioButton.git
+git clone https://Gypsyan:${GITHUB_TOKEN}@github.com/Gypsyan/KGRadioButton.git
 cd KGRadioButton
 git remote rm origin
-git remote add origin https://Gypsyan:${GITHUB_TOKEN}@github.com/Gypsyans/KGRadioButton.git
+git remote add origin https://Gypsyan:${GITHUB_TOKEN}@github.com/Gypsyan/KGRadioButton.git
 version=$(grep -o 'version.*=.*[0-9]' KGRadioButton.podspec | cut -f 2 -d "'")
 git fetch --tags
 if [[ ! "$(git tag)" =~ "${version}" ]]; then
